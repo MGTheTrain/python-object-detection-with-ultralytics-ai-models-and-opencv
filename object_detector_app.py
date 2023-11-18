717bb5507b427f1018e586c3a4caf1e72d78b1c8
unknown <Marvin.Gajek1@web.de>
2023-11-18 14:49:28 +0100

    update python code

--- object_detector_app.py
+++ object_detector_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from time import time
 from ultralytics import YOLO
+import matplotlib.pyplot as plt
 
 import supervision as sv
 
@@ -10,39 +11,26 @@ from ultralytics import RTDETR
 
 
 class ObjectDetection:
-
     def __init__(self, capture_index):
-       
         self.capture_index = capture_index
-        
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print("Using Device: ", self.device)
         
         self.model = YOLO("yolov8s.pt") # small
         # self.model = RTDETR("rtdetr-l.pt") # large 
         # self.model = RTDETR("rtdetr-x.pt") # extra large
-        
         self.CLASS_NAMES_DICT = self.model.model.names
-        
         print(self.CLASS_NAMES_DICT)
-    
         self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
     
-
-    
     def plot_bboxes(self, results, frame):
-        
         # Extract detections
-        
         boxes = results[0].boxes.cpu().numpy()
         class_id = boxes.cls
         conf = boxes.conf
         xyxy = boxes.xyxy
         
-    
         class_id = class_id.astype(np.int32)
-    
-        
         # Setup detections for visualization
         detections = sv.Detections(
                     xyxy=xyxy,
@@ -52,42 +40,47 @@ class ObjectDetection:
     
         # Format custom labels
         self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}" for xyxy, mask, confidence, class_id, track_id in detections]
-    
-        
         # Annotate and display frame
         frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
-        
         return frame
     
     
-    
-    def __call__(self):
+    def display_frame(self, frame):
+        # Convert BGR to RGB (OpenCV uses BGR, matplotlib uses RGB)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
 
+        cv2.imshow('Object Detector App', frame_rgb)
+        # # Display the frame using matplotlib
+        # plt.imshow(frame_rgb)
+        # plt.title('Object Detector App')
+        # plt.axis('off')  # Hide axes
+        # plt.show()
+
+    def __call__(self):
         cap = cv2.VideoCapture(self.capture_index)
         assert cap.isOpened()
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-      
+
         while True:
-          
             start_time = time()
-            
             ret, frame = cap.read()
-           
-            
+
             results = self.model.predict(frame)
             frame = self.plot_bboxes(results, frame)
-            
+
             end_time = time()
-            fps = 1/np.round(end_time - start_time, 2)
-             
-            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-            
-            cv2.imshow('YOLOv8 Detection', frame)
- 
+            fps = 1 / np.round(end_time - start_time, 2)
+
+            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+
+            # Display the frame using matplotlib
+            self.display_frame(frame)
+
             if cv2.waitKey(5) & 0xFF == 27:
                 break
-        
+
         cap.release()
         cv2.destroyAllWindows()
         
